a34925e16591a1d728242c42a468f36cc83073f1
Steven Weingärtner <pd@frozen-bits.de>
2021-06-16 22:01:08 +0200

    feat: allow previewd for video files in FilePicker

---
+++ README.md
@@ -0,0 +1,13 @@
+![Current release](https://img.shields.io/github/v/release/eXaminator/foundry-animation-preview?style=for-the-badge)
+![Supported Foundry Version](https://img.shields.io/badge/dynamic/json?label=Foundry&query=compatibleCoreVersion&url=https%3A%2F%2Fraw.githubusercontent.com%2FeXaminator%2Ffoundry-animation-preview%2Fmain%2Fsrc%2Fmodule.json&style=for-the-badge&color=ff6400)
+![Downloads](https://img.shields.io/github/downloads/eXaminator/foundry-animation-preview/total?style=for-the-badge)
+[![ko-fi](https://ko-fi.com/img/githubbutton_sm.svg)](https://ko-fi.com/X8X539NNJ)
+
+# Animation Preview
+This is a simple module for [Foundry Virtual Tabletop](http://foundryvtt.com) that allows you to preview animations
+(video files) in the file picker. This is especially helpful when looking for animated tokens or spell effects.
+
+Each video file will start playing when you hover the preview image with your mouse and stop playing once your mouse
+leaves the preview.
+
+![video](assets/animation-preview.mp4?raw=true)
---
+++ src/index.js
@@ -0,0 +1,36 @@
+Hooks.on('init', () => {
+    /*game.settings.register('animation-preview', '', {
+
+    });*/
+});
+
+Hooks.on('renderFilePicker', (filePicker, html) => {
+    html.find('[data-src="icons/svg/video.svg"]').each((idx, img) => {
+        const $img = $(img);
+        const $parent = $img.closest('[data-path]');
+        const path = $parent.data('path');
+        const width = $img.attr('width');
+        const height = $img.attr('height');
+        const $video = $(`<video preload="metadata" class="fas video-preview" src="${path}" loop width="${width}" height="${height}"></video>`);
+        $img.replaceWith($video);
+
+        const video = $video.get(0);
+
+        video.addEventListener('durationchange', () => {
+            video.currentTime = Math.round(video.duration / 2);
+        }, false);
+
+        $parent.addClass('video-parent');
+
+        $parent.hover(
+            () => {
+                video.currentTime = 0;
+                video.play().catch(() => null);
+            },
+            () => {
+                video.pause();
+                video.currentTime = Math.round(video.duration / 2);
+            },
+        );
+    });
+});
